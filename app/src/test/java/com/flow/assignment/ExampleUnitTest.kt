d2b5398bdd5e9215c82d6c96d571fef622298855
KOREAparksh <tmdgks2222@naver.com>
2023-01-19 21:48:01 +0900

    Test: 네이버영화 api 테스트 구현

--- app/src/test/java/com/flow/assignment/ExampleUnitTest.kt
+++ app/src/test/java/com/flow/assignment/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package com.flow.assignment
 
+import android.util.Log
+import com.flow.assignment.dto.MovieDto
+import com.flow.assignment.service.MovieService
+import com.flow.assignment.service.RetrofitClient
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import retrofit2.Response
+import retrofit2.Retrofit
 
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +19,21 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun retrofitTest() {
+        val retrofit: Retrofit = RetrofitClient.getInstance();
+        val movieService  = retrofit.create(MovieService::class.java)
+        var dto: MovieDto
+        val result = movieService.getMovies("아이언맨")
+        try {
+            val response: Response<MovieDto> = result.execute()
+            dto = response.body()!!
+        }catch (e: Exception){
+            dto = MovieDto("", 1,1,1)
+        }
+        println("123123")
+        println(dto.toString())
     }
 }
