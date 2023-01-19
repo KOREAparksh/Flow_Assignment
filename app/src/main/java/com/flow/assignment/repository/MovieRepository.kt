a7c7741e882a49d5772f17d586fa733e44cc3090
KOREAparksh <tmdgks2222@naver.com>
2023-01-20 05:17:35 +0900

    Feat: History Repository 구현

--- app/src/main/java/com/flow/assignment/repository/MovieRepository.kt
+++ app/src/main/java/com/flow/assignment/repository/MovieRepository.kt
@@ -4,14 +4,12 @@ import com.flow.assignment.dto.MovieDto
 import com.flow.assignment.service.api.MovieApi
 import com.flow.assignment.service.api.RetrofitClient
 import retrofit2.Retrofit
-import retrofit2.create
 
 class MovieRepository {
-    private var retrofit: Retrofit = RetrofitClient.getInstance()
-    private var movieApi: MovieApi =  retrofit.create(MovieApi::class.java)
+    private val retrofit: Retrofit = RetrofitClient.getInstance()
+    private val movieApi: MovieApi =  retrofit.create(MovieApi::class.java)
 
     fun getMovies(movieTitle: String) : MovieDto{
-        var movieDto: MovieDto
         val api = movieApi.getMovies(movieTitle)
         val response = api.execute()
 
@@ -19,7 +17,6 @@ class MovieRepository {
     }
 
     fun getMovies(movieTitle: String, startPoint: Int) : MovieDto{
-        var movieDto: MovieDto
         val api = movieApi.getMovies(movieTitle, startPoint)
         val response = api.execute()
 
