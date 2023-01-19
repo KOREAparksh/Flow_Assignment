d69b3d5487d438b1fe5f6d40808081e238dc32af
KOREAparksh <tmdgks2222@naver.com>
2023-01-19 23:06:50 +0900

    Feat: History Datebase 싱글톤class 구현

---
+++ app/src/main/java/com/flow/assignment/database/HistoryDatabase.kt
@@ -0,0 +1,36 @@
+package com.flow.assignment.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.flow.assignment.model.History
+import com.flow.assignment.service.dao.HistoryDao
+
+@Database(entities = [History::class], version = 1)
+abstract class HistoryDatabase : RoomDatabase() {
+    abstract fun historyDao() : HistoryDao
+
+    // 데이터 베이스 객체를 싱글톤으로 인스턴스.
+    companion object {
+        private var instance: HistoryDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): HistoryDatabase? {
+            if (instance == null)
+                synchronized(HistoryDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        HistoryDatabase::class.java,
+                        //Todo: 상수화
+                        "history.db"
+                    ).build()
+                }
+            return instance
+        }
+
+        fun destroyInstance() {
+            instance = null
+        }
+    }
+}
