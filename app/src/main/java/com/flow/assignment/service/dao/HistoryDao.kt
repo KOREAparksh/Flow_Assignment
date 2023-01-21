ea40869a501306e247e6f1f253dff70a19f8545c
KOREAparksh <tmdgks2222@naver.com>
2023-01-21 21:59:30 +0900

    Feat: 최근검색이력 내 10개 이상 저장 시 삭제기능 구현

--- app/src/main/java/com/flow/assignment/service/dao/HistoryDao.kt
+++ app/src/main/java/com/flow/assignment/service/dao/HistoryDao.kt
@@ -8,9 +8,18 @@ interface HistoryDao {
     @Query("SELECT * FROM history")
     suspend fun getAll(): List<History>
 
+    @Query("SELECT * FROM history LIMIT 1")
+    suspend fun getFirst(): History
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun saveHistory(history: History)
     
     @Delete
     suspend fun deleteHistory(history: History)
+
+    @Query("DELETE  FROM history")
+    suspend fun deleteAll()
+
+    @Query("SELECT COUNT(*) as cnt FROM history")
+    suspend fun countHistory(): Int
 }
